45eb38d5d25274bf17bd0dd3b24f012e5d70edca
Tim Hutchinson <t.hutchinson@utah.edu>
2016-12-07 15:26:07 -0500

    removed testing lines

--- 7/solution1.py
+++ 7/solution1.py
@@ -13,18 +13,15 @@ with open('input.txt') as f:
 
 count = 0
 for ip in ips:
-    p = True
     m = re.split(r'\[([^\]]+)\]', ip)
     parts = [' '.join(m[::2]), ' '.join(m[1::2])]
     for i in xrange(0,len(parts[0])-3):
         if check_abba(parts[0][i:i+4]):
             count += 1
-            p = False
             for j in xrange(0,len(parts[1])-3):
                 if check_abba(parts[1][j:j+4]):
                     count -= 1
                     break
             break
-    #if p: print	"%s\n" % ip
 
 print "Total is %s" % count
