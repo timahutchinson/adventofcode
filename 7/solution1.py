d92107f3d43185e25acd195ad0794e85599397c7
Tim Hutchinson <t.hutchinson@utah.edu>
2016-12-07 15:25:40 -0500

    bug fix

--- 7/solution1.py
+++ 7/solution1.py
@@ -16,15 +16,15 @@ for ip in ips:
     p = True
     m = re.split(r'\[([^\]]+)\]', ip)
     parts = [' '.join(m[::2]), ' '.join(m[1::2])]
-    for i in xrange(0,len(parts[0])-4):
+    for i in xrange(0,len(parts[0])-3):
         if check_abba(parts[0][i:i+4]):
             count += 1
             p = False
-            for j in xrange(0,len(parts[1])-4):
+            for j in xrange(0,len(parts[1])-3):
                 if check_abba(parts[1][j:j+4]):
                     count -= 1
                     break
             break
-    if p: print	"%s\n" % ip
+    #if p: print	"%s\n" % ip
 
 print "Total is %s" % count
