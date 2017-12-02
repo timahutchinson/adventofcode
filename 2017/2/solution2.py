beb6997ddda21fa9e32e299b8b5de5e8062c481e
Tim Hutchinson <tim.a.hutchinson@gmail.com>
2017-12-02 11:07:42 -0700

    day 2, part 2

---
+++ 2017/2/solution2.py
@@ -0,0 +1,14 @@
+from itertools import combinations
+
+with open('data.txt', 'rb') as f:
+    rows = [map(int, row.strip().split('\t')) for row in f.readlines()]
+
+total = 0
+for row in rows:
+    for pair in combinations(row,2):
+        if max(pair) % min(pair) == 0:
+            total += max(pair) / min(pair)
+        else:
+            pass
+
+print "Solution is %d" % total
