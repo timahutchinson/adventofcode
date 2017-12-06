e3f0c5cc68c38e392d85d302fb4ebf9948a896d5
Tim Hutchinson <tim.a.hutchinson@gmail.com>
2017-12-05 17:21:57 -0700

    python day 5 puzzle 2

---
+++ python/2017/5/solution2.py
@@ -0,0 +1,17 @@
+with open("../../../data/2017/5/data.txt", "rb") as f:
+    vals = [int(line.replace('\n','')) for line in f.readlines()]
+
+count = 0
+ind = 0
+while True:
+    old_val = vals[ind]
+    if old_val >= 3:
+        vals[ind] -= 1
+    else:
+        vals[ind] += 1
+    count += 1
+    ind += old_val
+    if ind >= len(vals):
+        break
+
+print "Solution is %d" % count
