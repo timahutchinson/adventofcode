f1ed4508d9c6e1319e30394b61d7c4ed7d30e07e
Tim Hutchinson <tim.a.hutchinson@gmail.com>
2017-12-13 19:38:25 -0700

    added data and python day 10 puzzle 1

---
+++ python/2017/10/solution1.py
@@ -0,0 +1,27 @@
+with open("../../../data/2017/10/data.txt", "rb") as f:
+    steps = map(int, f.read().strip().split(','))
+
+#steps = [3,4,1,5]
+
+nums = range(256)
+nnums = len(nums)
+pos = 0
+skip = 0
+
+for step in steps:
+    #import pdb; pdb.set_trace()
+    if (pos + step) >= nnums:
+        templist = nums[pos:] + nums[:step - (nnums - pos)]
+        templist_r = list(reversed(templist))
+        nums[pos:] = templist_r[:nnums - pos]
+        nums[:step - (nnums - pos)] = templist_r[nnums - pos:]
+    else:
+        templist = nums[pos:pos + step]
+        templist_r = list(reversed(templist))
+        nums[pos:pos + step] = templist_r
+    pos += (step + skip)
+    if pos > nnums:
+        pos -= nnums
+    skip += 1
+
+print "Solution is %d" % (nums[0] * nums[1])
