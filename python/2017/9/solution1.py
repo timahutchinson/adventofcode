178d175db4b7e83d5a8aaf449c64e7b63d8da93f
Tim Hutchinson <tim.a.hutchinson@gmail.com>
2017-12-13 18:51:14 -0700

    added data and python day 9 puzzle 1

---
+++ python/2017/9/solution1.py
@@ -0,0 +1,21 @@
+import re
+
+with open("../../../data/2017/9/data.txt", "rb") as f:
+    stream = f.read().strip()
+
+# First, remove all canceled characters
+stream = re.sub('!.', '', stream)
+
+# Next, all garbage
+stream = re.sub('<[^>]*>', '', stream2)
+
+# Score groups
+score = 0
+value = 0
+for char in stream:
+    if char == '{':
+        value += 1
+    elif char == '}':
+        score += value
+        value -= 1
+print "Solution is %d" % score
