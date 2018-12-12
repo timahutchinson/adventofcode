79a5f8bfe4040b04b7baa07caa4f5579766a9026
Tim Hutchinson <tim.a.hutchinson@gmail.com>
2018-12-11 19:52:35 -0700

    added day9 sol2

---
+++ python/2018/9/solution2.py
@@ -0,0 +1,24 @@
+import re
+from collections import defaultdict, deque
+
+with open("../../../data/2018/9/data.txt") as f:
+    p = re.compile("(\d+) players; last marble is worth (\d+) points")
+    nelves, nmarbles = map(int, p.findall(f.read().strip())[0])
+
+nmarbles *= 100
+
+imarb = 0
+played = deque([0])
+scores = defaultdict(lambda: 0)
+while imarb < nmarbles:
+    imarb += 1
+    if imarb % 23 == 0:
+        scores[imarb % nelves] += imarb
+        played.rotate(7)
+        scores[imarb % nelves] += played.pop()
+        played.rotate(-1)
+    else:
+        played.rotate(-1)
+        played.append(imarb)
+
+print(max(scores.values()))
