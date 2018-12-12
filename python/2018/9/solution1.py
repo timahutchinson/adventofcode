b468739472b3117993ebabc30b428a58a88b7e22
Tim Hutchinson <tim.a.hutchinson@gmail.com>
2018-12-11 19:48:52 -0700

    day9 sol1 now works

--- python/2018/9/solution1.py
+++ python/2018/9/solution1.py
@@ -1,32 +1,22 @@
 import re
+from collections import defaultdict, deque
 
 with open("../../../data/2018/9/data.txt") as f:
     p = re.compile("(\d+) players; last marble is worth (\d+) points")
     nelves, nmarbles = map(int, p.findall(f.read().strip())[0])
 
-nelves, nmarbles = 13, 7999
-
 imarb = 0
-current = 0
-played = [0]
-elf = -1
-scores = [0] * nelves
+played = deque([0])
+scores = defaultdict(lambda: 0)
 while imarb < nmarbles:
     imarb += 1
-    elf += 1
-    if elf >= nelves:
-        elf -= nelves
     if imarb % 23 == 0:
-        scores[elf] += imarb
-        current -= 7
-        while current >= len(played):
-            current -= len(played)
-        scores[elf] += played.pop(current)
+        scores[imarb % nelves] += imarb
+        played.rotate(7)
+        scores[imarb % nelves] += played.pop()
+        played.rotate(-1)
     else:
-        current += 2
-        while current > len(played):
-            current -= len(played)
-        played.insert(current, imarb)
-    #print(played)
+        played.rotate(-1)
+        played.append(imarb)
 
-print(max(scores))
+print(max(scores.values()))
